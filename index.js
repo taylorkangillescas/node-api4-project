9a8e35148c0b6482e0cd08a2239c903cf39aa10c
Taylor Kang-Illescas <t.kangillescas@gmail.com>
2021-07-08 19:22:11 -0700

    deployment 2

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/", (_,res) => {
+    res.json({data:"API is accounted for"})
+})
+
+app.listen(port,() =>{
+    console.log(`Server is alive on port ${port}`)
+})
 
 console.log("Its alive")
 console.log(__dirname)
